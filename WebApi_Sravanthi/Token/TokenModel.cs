df0049c7503cbfe1dacf97024607e9d00163d2f9
SRAVANTHI BADANAKANTI <srdigi1619@gmail.com>
2025-10-24 15:25:33 +0530

    Done with Refresh token generation

---
+++ WebApi_Sravanthi/Token/TokenModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi_Sravanthi.Token
+{
+    // ✅ Separate models, not inside TokenModel class
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RefreshRequest
+    {
+        public string OldToken { get; set; }
+        public string RefToken { get; set; }
+    }
+}
